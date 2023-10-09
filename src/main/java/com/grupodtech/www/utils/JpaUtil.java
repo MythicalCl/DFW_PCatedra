126e056d9eed4c721c3fbe27037ff6f9218b082e
MythicalCl <CB121033@alumno.udb.edu.sv>
2023-10-09 16:55:01 -0600

    Commit 1

---
+++ src/main/java/com/grupodtech/www/utils/JpaUtil.java
@@ -0,0 +1,14 @@
+package com.grupodtech.www.utils;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+public class JpaUtil {
+    private static final EntityManagerFactory emFactory;
+    static {
+        emFactory = Persistence.createEntityManagerFactory("default");
+    }
+    public static EntityManager getEntityManager() {
+        return emFactory.createEntityManager();
+    }
+}
